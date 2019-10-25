0a580902f797541d9a82cbc1a07b5dc0d40a8b91
mattiagallistru01 <mattiagallistru.mg@gmail.com>
2019-10-25 10:56:10 +0000

    turchese

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var app = express();
+const people = require('./people.json');
+
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
-  res.send('voglio morire');
+  res.send('Hello World!');
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
 });
